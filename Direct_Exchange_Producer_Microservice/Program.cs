6a807a8a0ba0d421f5250831c6edd67dde2aec11
bright charway <charway30@gmail.com>
2024-10-23 14:25:36 +0200

    rabbitmq first

--- Direct_Exchange_Producer_Microservice/Program.cs
+++ Direct_Exchange_Producer_Microservice/Program.cs
@@ -12,16 +12,20 @@ namespace Direct_Exchange_Producer_Microservice
             using (var channel = connection.CreateModel())
             {
                 channel.ExchangeDeclare(exchange: "direct_logs", type: ExchangeType.Direct);
-
+                int num = 10;
                 string severity = "error";
-                string message = "Dette er en fejlmeddelelse.";
-                var body = Encoding.UTF8.GetBytes(message);
+                string message = $"Dette er en fejlmeddelelse nr {num}!! Bright kan ikke finde ud af at kode.";
 
-                channel.BasicPublish(exchange: "direct_logs",
-                                     routingKey: severity,
-                                     basicProperties: null,
-                                     body: body);
-                Console.WriteLine("[x] Sent '{0}':'{1}'", severity, message);
+                for (int i = 0; i < num; i++)
+                {
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "direct_logs",
+                                         routingKey: severity,
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine("[x] Sent '{0}':'{1}'", severity, message);
+                }
             }
         }
     }
